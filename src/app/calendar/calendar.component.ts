619332817e9a37a3fe9aba9eed7d17f2f912c39a
Hato <fansintuesan@gmail.com>
2024-05-27 16:31:19 +0800

    feat: calendar page basic layout

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDatepickerModule, MatListModule, MatDividerModule],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.scss',
+  providers: [provideNativeDateAdapter()]
 })
 export class CalendarComponent {
-
+  selected: Date | null = new Date();
 }
