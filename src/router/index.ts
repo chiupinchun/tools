772273d61b215c8132606b112b78ca5efe51c6e5
hatochiu <hatochiu@welltend.com.tw>
2025-08-27 08:52:38 +0800

    revert: not need signup anymore

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
-import { useUserStore } from "@/stores/user";
-import Signup from "@/views/Signup.vue";
+import Todo from "@/views/Todo.vue";
 
 const routes = [
-  { path: "/signup", component: Signup },
+  { path: "/todo", component: Todo },
   { path: "/", component: Home },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
-
-router.beforeEach((to) => {
-  const userStore = useUserStore();
-  if (!userStore.isLogin && to.path !== "/signup") {
-    return "/signup";
-  }
 });
 
 export { router };
